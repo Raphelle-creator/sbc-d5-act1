edb2455385a250ab36e5372f38af073f13d4c91b
Raphelle <raphelle.lansaderas@aclcbutuan.edu.ph>
2024-06-24 04:41:19 -0700

    Add files via upload

---
+++ sbc-d5-act1.py
@@ -0,0 +1,20 @@
+numbers = []
+
+for i in range(3):
+    num = int(input(f"Enter Number {i+1}: "))
+    numbers.append(num)
+
+print(f"All entered numbers: {numbers}")
+
+while numbers:
+    user= int(input("Is the boss here? Enter 0 for No, 1 for Yes: "))
+    
+    if user == 0:
+        numbers.pop()
+    elif user == 1:
+        numbers.pop(0)
+    else:
+        print("Invalid input. Please enter 0 for No or 1 for Yes.")  
+    print(f"Updated numbers: {numbers}")
+
+print("No more numbers left.")
